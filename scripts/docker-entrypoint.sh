5d048f5257e7f806f875e2f543e9b6ac0ad0fb98
CheariX <code@chearix.de>
2026-05-07 15:44:13 +0200

    fix: :bug: listen on every port (not sure if we really need it)

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -9,11 +9,11 @@ fluxbox &
 sleep 1
 
 # Start x11vnc with proper settings for single desktop
-x11vnc -display :99 -nopw -listen localhost -xkb -forever -shared -rfbport 5900 -repeat -nowcr -nowf -noxdamage &
+x11vnc -display :99 -nopw -listen 0.0.0.0 -xkb -forever -shared -rfbport 5900 -repeat -nowcr -nowf -noxdamage &
 sleep 2
 
 # Start noVNC
-/opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
+/opt/novnc/utils/novnc_proxy --vnc 0.0.0.0:5900 --listen 6080 &
 sleep 2
 
 # Start Flask web application
